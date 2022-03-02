7685338d96260757e62a0cb8531d5f5894134d7f
Kevin Qi <kkevinqi@gmail.com>
2022-03-01 22:07:44 -0500

    index page and php update

--- Project/post.php
+++ Project/post.php
@@ -7,10 +7,26 @@
         $rname = $_POST['rname'];
         $review = $_POST['review'];
 
-        echo "Thanks for the Review on, $rname <br/>";
-        echo "<i> $review</i> <br/>";
+        echo "Thanks for the Review on restaurant $rname <br/>";
+        echo "Your review <br/>";
+        echo "<i> $review</i> <br/> is saved <br/>";
+        echo "redirecting you back in 5 seconds";
     }
     ?>
+    <script type="text/javascript"> 
+       var num = 4;
+		var URL = "index.html";
+        window.setTimeout("doUpdate()", 1000);
+        function doUpdate(){
+			if(num != 0){
+				num --;
+				window.setTimeout("doUpdate()", 1000);
+			}else{
+				num = 4;
+				window.location = URL;
+			}
+		}
+    </script>
 </body>
-
+    
 </html>
