7e8c86b84e875ada4ac60dce2ee38be23b0f5667
madisenbpatrick <mbp4wwc@virginia.edu>
2022-04-03 16:48:16 -0400

    what should I do page loads random review

--- Project/classes/UvaMoves.php
+++ Project/classes/UvaMoves.php
@@ -288,8 +288,38 @@ class UvaMoves{
     }
 
     private function what(){
+        $uvaMoves_whatReview = $this->loadWhat();
+
+        $user = [
+            "name" => $_COOKIE["name"],
+            "email" => $_COOKIE["email"],
+            "id" => $_COOKIE["id"],
+        ];
+
+        // $data = $this->db->query("select * from uvaMoves_reviews where category = ? order by rand();","s","r_restaurant");
+
         include("templates/what.php");
     }
 
+    private function loadWhat(){
+
+        $user = [
+            "name" => $_COOKIE["name"],
+            "email" => $_COOKIE["email"],
+            "id" => $_COOKIE["id"],
+        ];
+
+        // $data = $this->db->query("select * from uvaMoves_reviews where category = ? order by rand();","s","r_restaurant");
+
+        $data = $this->db->query("select * from uvaMoves_reviews order by rand() limit 1;");
+
+        if (!isset($data[0])) {
+            die("No questions in the database");
+        }
+        //$question = $data[0];
+        return $data;
+        
+    }
 
 }
+
